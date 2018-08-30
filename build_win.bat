b3962e63701cd52e415d3d86bfffddaa9e129820
Prajeen Govardhanam <prajeengovardhanam@gmail.com>
2018-08-31 00:13:16 +0530

    Enhancements

--- build_win.bat
+++ build_win.bat
@@ -1,3 +1,3 @@
 @cmake.exe -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -H. -B build
-@cmake.exe --build build --target ../bin/Cipher-Encryption-Project.exe -- -j4
+@cmake.exe --build build --target ../bin/Cipher-Encryption-Project.exe
 "./bin/Cipher-Encryption-Project.exe"
