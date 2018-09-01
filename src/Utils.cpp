1c6d8c9db65a57a7d4caca26de062e20acae6d2c
Prajeen Govardhanam <prajeengovardhanam@gmail.com>
2018-09-01 12:04:45 +0530

    REMOVED : unused functions code

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,31 +1,18 @@
+//Constants
+#define SCREEN_WIDTH 80
+#ifdef _WIN32
+#define CLEAR "cls"
+#else //In any other OS
+#define CLEAR "clear"
+#endif
 #include "Utils.h"
 
-void print(char ch)
-{
-    std::cout << ch;
-}
-
-void print(int i)
-{
-    std::cout << i;
-}
-
-void print(float f)
-{
-    std::cout << f;
-}
-
-void print(double d)
-{
-    std::cout << d;
-}
-
-void print(std::string &content)
+void print(const char *content)
 {
     std::cout << content;
 }
 
-void print(const char *content)
+void print(std::string &content)
 {
     std::cout << content;
 }
@@ -35,32 +22,12 @@ void print(const std::string *content)
     std::cout << *content;
 }
 
-void println(char ch)
-{
-    std::cout << ch << std::endl;
-}
-
-void println(int i)
-{
-    std::cout << i << std::endl;
-}
-
-void println(float f)
-{
-    std::cout << f << std::endl;
-}
-
-void println(double d)
-{
-    std::cout << d << std::endl;
-}
-
-void println(std::string &content)
+void println(const char *content)
 {
     std::cout << content << std::endl;
 }
 
-void println(const char *content)
+void println(std::string &content)
 {
     std::cout << content << std::endl;
 }
@@ -70,33 +37,9 @@ void println(const std::string *content)
     std::cout << *content << std::endl;
 }
 
-void printcnt(char content)
+void printcnt(const char *content)
 {
-    int length = 1;
-    int position = static_cast<int>((SCREEN_WIDTH - length) / 2);
-    std::cout << std::string(position - 1, ' ');
-    println(content);
-}
-
-void printcnt(int content)
-{
-    int length = toString(content).size();
-    int position = static_cast<int>((SCREEN_WIDTH - length) / 2);
-    std::cout << std::string(position - 1, ' ');
-    println(content);
-}
-
-void printcnt(float content)
-{
-    int length = toString(content).size();
-    int position = static_cast<int>((SCREEN_WIDTH - length) / 2);
-    std::cout << std::string(position - 1, ' ');
-    println(content);
-}
-
-void printcnt(double content)
-{
-    int length = toString(content).size();
+    int length = strlen(content);
     int position = static_cast<int>((SCREEN_WIDTH - length) / 2);
     std::cout << std::string(position - 1, ' ');
     println(content);
@@ -105,14 +48,6 @@ void printcnt(double content)
 void printcnt(std::string &content)
 {
     int length = content.size();
-    int position = static_cast<int>((SCREEN_WIDTH - length) / 2);
-    std::cout << std::string(position - 1, ' ');
-    println(content);
-}
-
-void printcnt(const char *content)
-{
-    int length = strlen(content);
     int position = static_cast<int>((SCREEN_WIDTH - length) / 2);
     std::cout << std::string(position - 1, ' ');
     println(content);
@@ -136,24 +71,9 @@ bool areTheSame(char a, char b)
     return a == b;
 }
 
-bool areTheSame(float a, float b)
-{
-    return a == b;
-}
-
-bool areTheSame(double a, double b)
-{
-    return a == b;
-}
-
 bool areTheSame(std::string &a, std::string &b)
 {
     return a == b;
-}
-
-bool areTheSame(char *a, char *b)
-{
-    return strcmp(a, b) == 0;
 }
 
 bool areTheSame(std::string *a, std::string *b)
